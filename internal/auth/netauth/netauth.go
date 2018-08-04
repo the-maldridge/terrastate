bb9ebf2a2e2eefe5e919fc1dc3662c8269c027a4
Michael Aldridge <aldridge.mac@gmail.com>
2018-08-04 01:28:29 -0700

    Permit restriction to a NetAuth group

--- internal/auth/netauth/netauth.go
+++ internal/auth/netauth/netauth.go
@@ -1,11 +1,18 @@
 package netauth
 
 import (
+	"flag"
+	"log"
+
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
 	"github.com/NetAuth/NetAuth/pkg/client"
 	"github.com/the-maldridge/TerraState/internal/auth"
+)
+
+var (
+	reqGroup = flag.String("required_group", "", "Required group for use of TerraState")
 )
 
 func init() {
@@ -40,5 +47,22 @@ func (b *netAuthBackend) AuthUser(user, pass string) error {
 	if status.Code(err) != codes.OK || !result.GetSuccess() {
 		return auth.ErrUnauthenticated
 	}
-	return nil
+
+	if *reqGroup == "" {
+		return nil
+	}
+
+	groups, err := b.nacl.ListGroups(user, true)
+	if status.Code(err) != codes.OK {
+		log.Println("RPC Error: ", err)
+		return auth.ErrUnauthenticated
+	}
+
+	for _, g := range groups {
+		if g.GetName() == *reqGroup {
+			return nil
+		}
+	}
+
+	return auth.ErrUnauthenticated
 }
